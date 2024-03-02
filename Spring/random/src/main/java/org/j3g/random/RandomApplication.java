60b81a0c205753e540e648b9602c856d5fad1093
mathgame2 <20twu@student.aisb.hu>
2024-03-02 10:29:23 +0000

    Adding spring initializr stuff

---
+++ Spring/random/src/main/java/org/j3g/random/RandomApplication.java
@@ -0,0 +1,13 @@
+package org.j3g.random;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RandomApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RandomApplication.class, args);
+	}
+
+}
---
+++ Spring/random/src/test/java/org/j3g/random/RandomApplicationTests.java
@@ -0,0 +1,13 @@
+package org.j3g.random;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RandomApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
