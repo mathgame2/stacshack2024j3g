cbfb75b8c8576835377f6cf211eaf174debdef39
Martin Lee <mhl7@st-andrews.ac.uk>
2024-03-02 16:29:41 +0000

    box

--- JS/app/src/Body.js
+++ JS/app/src/Body.js
@@ -4,7 +4,8 @@ import Map from './Map';
 
 const Body = ({ atmData }) => {
     const [items, setItems] = useState([]);
-
+    let atmDataCopy = atmData
+    // console.log(atmDataCopy.length)
     useEffect(() => {
         let accFilter = new Set();
         
@@ -23,18 +24,35 @@ const Body = ({ atmData }) => {
 
             setItems(accList);
         }
-    }, [atmData]); // Depend on atmData so it updates when atmData changes
-    
+    }, [atmData]);
+
     const toggleCheckbox = (id) => {
         setItems(items.map((item) =>
             item.id === id ? { ...item, checked: !item.checked } : item
         ));
     };
+
+    const filterAtm = (item) => {
+        toggleCheckbox(item.id)
+        filterData(item.text)
+        console.log('filterAtm', atmDataCopy.length)
+    }
+
+    const filterData = (text) => {
+        atmDataCopy = []
+        for (let k in atmData) {
+            for (let l in atmData[k].accessibility) {
+                if(atmData[k].accessibility[l] === text) {
+                    atmDataCopy.push(atmData[k])
+                }
+            }
+        }
+    }
     
     return (
         <div className={styles.bodyBox}>
             <div className={styles.mapBox}>
-                <Map atmData={atmData} />
+                <Map atmData={atmDataCopy} />
             </div>
             <div className={styles.menuBox}>
                 {items.map((item) => (
@@ -42,7 +60,7 @@ const Body = ({ atmData }) => {
                         <input
                             type="checkbox"
                             checked={item.checked}
-                            onChange={() => toggleCheckbox(item.id)}
+                            onChange={() => filterAtm(item)}
                         />
                         {item.text}
                     </label>
