4cefaab0fd91c4623bc84cbc5c330be02b107a2c
YIYAO ZHANG <yz229@st-andrews.ac.uk>
2024-03-02 20:46:44 +0000

    add dancing bill

--- JS/app/src/HiddenEffect.js
+++ JS/app/src/HiddenEffect.js
@@ -1,33 +1,36 @@
-import React, { useState, useEffect } from 'react';
-import NotBill from './pics/not_bill.png';
+import React, { useState, useEffect, useRef } from 'react';
+import NotBill from './pics/not_bill_dancing.gif';
 import './HiddenEffect.css'; 
-import yourMusicFile from './music/GravityFalls.mp3'; // Import your audio file
+import yourMusicFile from './music/GravityFalls.mp3'; 
 
 const HiddenEffect = ({ onPasswordMatch }) => {
   let flag = [];
   const [showImage, setShowImage] = useState(false);
+  const [playAudio, setPlayAudio] = useState(false);  // State to control audio playback
   const passwordSequence = ["ArrowUp", "ArrowUp", "ArrowDown", "ArrowDown", "ArrowLeft", "ArrowRight", "ArrowLeft", "ArrowRight", "b", "a"];
 
-  // Ref for the audio element
-  const audioRef = React.createRef();
+  const audioRef = useRef(null); // Changed to useRef
 
   const handleKeyPress = (event) => {
     flag.push(event.key);
 
-    if(flag.length !== 0){
-        setTimeout(() => {
-            flag = []; 
-            console.log("reset")
-          }, 3000);
+    if (flag.length !== 0) {
+      setTimeout(() => {
+        flag = [];
+        console.log("reset");
+      }, 3000);
     }
-   
+
     if (flag.length === passwordSequence.length) {
       if (flag.every((value, index) => value === passwordSequence[index])) {
         console.log("Password entered correctly");
         setShowImage(true);
-        setTimeout(() => setShowImage(false), 3000); // Keep image visible for 5 seconds
-        onPasswordMatch(true); 
-        audioRef.current.play(); // Play the audio
+        setPlayAudio(true); // Set the state to play audio
+        setTimeout(() => {
+          setShowImage(false);
+          setPlayAudio(false); // Stop playing after a delay
+        }, 3000);
+        onPasswordMatch(true);
       } else {
         console.log("Incorrect password");
         onPasswordMatch(false);
@@ -42,12 +45,18 @@ const HiddenEffect = ({ onPasswordMatch }) => {
     return () => {
       window.removeEventListener('keydown', handleKeyPress);
     };
-  }, []); 
+  }, []);
+
+  useEffect(() => {
+    if (playAudio && audioRef.current) {
+      audioRef.current.play();
+    }
+  }, [playAudio]); // Play audio when playAudio state changes
 
   return (
     <>
       {showImage && <img src={NotBill} alt="Not Bill" className="fade-in-out" />}
-      <audio ref={audioRef} src={yourMusicFile} />
+      {playAudio && <audio ref={audioRef} src={yourMusicFile} />}
     </>
   );
 };
