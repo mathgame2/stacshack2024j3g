04b85731a67cf84aed37f98e3269a8009040ff05
Martin Lee <mhl7@st-andrews.ac.uk>
2024-03-02 11:03:58 +0000

    landing page

---
+++ JS/app/src/Body.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Body = () => {
+    return (
+        <div style={{ padding: '20px' }}>
+            <p>This is the body of the landing page. Put your content here.</p>
+        </div>
+    );
+};
+
+export default Body;
