419d90177e99b986d3b50cb4ee9e376f45aff9b5
YIYAO ZHANG <yz229@st-andrews.ac.uk>
2024-03-02 15:58:29 +0000

    add voronoi

--- JS/app/src/Map.js
+++ JS/app/src/Map.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import L from 'leaflet';
 import 'leaflet/dist/leaflet.css';
 import ATMIcon from './pics/ATM_mark.png';
+import { voronoi } from 'd3-voronoi';
 
 class Map extends Component {
     
@@ -9,6 +10,7 @@ class Map extends Component {
         super(props);
         this.mapRef = React.createRef();
         this.map = null; 
+        this.voronoiLayer = null;
     }
 
     componentDidMount() {
@@ -30,7 +32,6 @@ class Map extends Component {
 
     addAtmMarkers() {
         const { atmData } = this.props;
-        console.log('ATM Data:', atmData);
     
         if (atmData && Array.isArray(atmData)) {
             const customIcon = L.icon({
@@ -42,9 +43,6 @@ class Map extends Component {
             atmData.forEach(atm => {
                 const { coords, accessibility, id } = atm;
                 const { Latitude, Longitude } = coords;
-    
-                console.log('Adding marker at:', Latitude, Longitude);
-    
                 if (Latitude && Longitude) {
                     // Create a popup with additional information
                     const popupContent = `
@@ -57,11 +55,37 @@ class Map extends Component {
                     marker.bindPopup(popupContent); // Bind popup to marker
                 }
             });
+
+            // Generate and add Voronoi diagram
+            this.generateVoronoi();
         }
     }
-    
-    
 
+    generateVoronoi() {
+        const { atmData } = this.props;
+        const points = atmData.map(atm => {
+            const { coords } = atm;
+            const { Latitude, Longitude } = coords;
+            return [Longitude, Latitude]; // Swap Longitude and Latitude for d3-voronoi
+        });
+
+        const voronoiGenerator = voronoi().extent([[-1000, -1000], [this.map.getSize().x, this.map.getSize().y]]);
+        const voronoiPolygons = voronoiGenerator(points).polygons();
+
+        if (this.voronoiLayer) {
+            this.voronoiLayer.remove();
+        }
+
+        this.voronoiLayer = L.layerGroup();
+
+        voronoiPolygons.forEach(polygon => {
+            const latlngs = polygon.map(point => [point[1], point[0]]); // Swap back to [Latitude, Longitude]
+            L.polygon(latlngs, { color: 'blue' }).addTo(this.voronoiLayer);
+        });
+
+        this.voronoiLayer.addTo(this.map);
+    }
+    
     render() {
         return <div ref={this.mapRef} style={{ width: '100%', height: '100%' }} />;
     }
