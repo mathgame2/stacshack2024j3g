6e8b1719dd3d8568c084056141d0f626dca2f5c4
YIYAO ZHANG <yz229@st-andrews.ac.uk>
2024-03-02 21:12:14 +0000

    change front end port

--- Spring/random/src/main/java/org/j3g/random/API.java
+++ Spring/random/src/main/java/org/j3g/random/API.java
@@ -17,21 +17,21 @@ public class API {
      *
      * @return String content of the atm data file
      */
-    @CrossOrigin(origins = "http://localhost:3000")
+    @CrossOrigin(origins = "http://localhost:22489")
     @GetMapping("api/get_atm_data")
     String getAtmData() throws IOException {
         File file = new File(getClass().getResource("/data/atms.json").getFile());
         return Files.readString(file.toPath());
     }
 
-    @CrossOrigin(origins = "http://localhost:3000")
+    @CrossOrigin(origins = "http://localhost:22489")
     @GetMapping("api/get_geo_coords_atm")
     GeographicCoordinates[] getGeoCoordsAtm() throws IOException {
         DataManager manager = new DataManager();
         return manager.read_geocoords_atm();
     }
 
-    @CrossOrigin(origins = "http://localhost:3000")
+    @CrossOrigin(origins = "http://localhost:22489")
     @GetMapping("api/get_atms")
     ATM[] getAtms() throws IOException {
         DataManager manager = new DataManager();
@@ -41,7 +41,7 @@ public class API {
      *
      * @return String content of the branch data file
      */
-    @CrossOrigin(origins = "http://localhost:3000")
+    @CrossOrigin(origins = "http://localhost:22489")
     @GetMapping("api/get_branch_data")
     String getBranchData() throws IOException {
         File file = new File(getClass().getResource("/data/atms.json").getFile());
