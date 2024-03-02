cce66cee09a5858675d5111eeb6de5e1ad9b24b5
Martin Lee <mhl7@st-andrews.ac.uk>
2024-03-02 11:29:35 +0000

    header

--- JS/app/src/App.js
+++ JS/app/src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import Body from './Body'; 
+import Header from './Header';
 import Map from './Map';
 
 function App() {
   return (
     <div className="App">
+      <Header/>
       <Body />
     </div>
   );
